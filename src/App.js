076fc82eb1c151468b7847dc5c7e9e6a8513f0a9
Mo-Jo <mo.juseph@gmail.com>
2019-09-04 02:59:29 -0400

    updated the shuffle

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ class App extends Component {
     highScore: 0,
     maxScore: 12
   };
-  handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ score: this.state.score + 1 });
+  // };
 
   // shuffle = (friends)=> {
   //     let j, x, i;
@@ -37,12 +37,61 @@ class App extends Component {
   //         friends[j] = x;
   //     }
   // }
+  // gameOver = () => {
+  //   if (this.state.score > this.state.highscore) {
+  //     this.setState({highscore: this.state.score}, function() {
+  //       console.log(this.state.highscore);
+  //     });
+  //   }
+  //   this.state.cards.forEach(friend => {
+  //     friend.count = 0;
+  //   });
+  //   alert(`Game Over :( \nscore: ${this.state.score}`);
+  //   this.setState({score: 0});
+  //   return true;
+  // }
   
+  shuffle = function shuffle(friends) {
+    var i,
+        j,
+        temp;
+    for (i = friends.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = friends[i];
+        friends[i] = friends[j];
+        friends[j] = temp;
+    }
+    return friends;    
+};
 
+  click = id => {
+    const shuffles = this.shuffle(friends);
+    this.setState({friends: shuffles});
+
+    // const over = this.gameOver();
+    // this.setState({friends: over});
+
+    if (this.state.id){
+      console.log(this.click)
+      this.setState({
+        score: this.state.score + 1,
+        clicked: true,
+      });
+    }else{
+      this.setState({
+        clicked:true,
+        score: this.state.score+1
+      });
+      // this.state.gameOver();
+    }
+    if (this.state.score > this.state.highScore){
+      this.setState({highScore: this.state.score});
+    }
+  }
   // Map over this.state.friends and render a FriendCard component for each friend object
   // gameOver = () =>{
-  //   if({this.state.score}>12){
-
+  //   if(this.state.score > 12){
+  //     alert("gameOver");
   //   }
   // }
   render() {
@@ -54,9 +103,11 @@ class App extends Component {
     </nav>
         <h1 className="title">Clicky-Game</h1>
         {this.state.friends.map(friend => (
-         <button onClick={this.handleIncrement}>
+         <button onClick={this.click}>
          <FriendCard
             // shuffle = {this.shuffle}
+            // click= {this.click}
+            // gameOver = {this.gameOver}
             id={friend.id}
             key={friend.id}
             name={friend.name}
